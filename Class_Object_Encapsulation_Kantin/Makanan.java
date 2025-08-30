a0202156e211da7ca94523b2a38dfcadf100cd7b
Ersya Hasby Satria <hasbysatria54@gmail.com>
2025-08-30 20:26:02 +0700

    Program Class Makanan

---
+++ Class_Object_Encapsulation_Kantin/Makanan.java
@@ -0,0 +1,21 @@
+package com.tugas;
+
+public class Makanan {
+    private String nama;
+    private int harga;
+
+    public Makanan(String nama, int harga) {
+        this.nama = nama;
+        this.harga = harga;
+    }
+
+    public String getNama() {
+        return nama;
+    }
+
+    public int getHarga() {
+        return harga;
+    }
+}
+
+
